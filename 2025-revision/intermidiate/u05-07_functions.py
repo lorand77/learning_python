c4ad5c5a170b104b1936cd02260bfa8091aa66e1
lorand <lorand77@users.noreply.github.com>
2025-12-14 17:11:57 +0100

    drawing cicles

--- 2025-revision/intermidiate/u05-07_functions.py
+++ 2025-revision/intermidiate/u05-07_functions.py
@@ -4,12 +4,24 @@
 # Uses string multiplication
 # Calls the function at least once
 
+from math import sqrt
+
 def draw_square(height, paint="X"): #only works well for specific squares
     for i in range(height):
         print(paint * int(height * 2.2))
 
-print(draw_square(10))
-print(draw_square(10, "X"))
-print(draw_square(10, "O"))
-print(draw_square(4))
-print(draw_square(8))
+def draw_circle(radius, paint="X"):
+    scale = 2.2    
+    for y in range(radius, -radius-1, -1):
+        x = round(scale * sqrt(radius ** 2 - y ** 2))
+        print(" " * round(radius * scale - x) + paint * 2 * x)
+
+
+draw_square(10)
+draw_square(10, "X")
+draw_square(10, "O")
+draw_square(4)
+draw_square(8)
+
+draw_circle(8)
+draw_circle(16)
