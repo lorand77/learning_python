a9726f0286660c1c37b71da1ce2e3108b90a81ca
lorand <lorand77@users.noreply.github.com>
2025-12-22 11:22:20 +0100

    mutable vs immutable

--- 2024_b-fall/mutable.py
+++ 2024_b-fall/mutable.py
@@ -1,32 +1,36 @@
-# a = [0]*6
-# x = []
-# for i in range(6):
-#     x.append(a.copy())
+a = [0]*6
+x = []
+for i in range(3):
+    x.append(a.copy())
 
-# print(x)
+print(x)
 
-# x[0][0]=1
-# print(x)
+x[0][0]=1
+print(x)
 
-# x = 2
+x = 2
 
-# def f(a):
-#     print(a)
-#     a = 3
-#     print(a)
+def f(a):
+    print(a)
+    a = 3
+    print(a)
 
-# f(x)
-# print(x)
+f(x)
+print(x)
 
 
-x = [1,2]
+
 
 def f(a):
     print(a)
     a[0] = 0
     print(a)
 
+x = [1,2]
 f(x)
+print(x)
+
+x = [1,2]
 f(x.copy())
 print(x)
 
--- 2024_b-fall/mutable2.py
+++ 2024_b-fall/mutable2.py
@@ -10,4 +10,24 @@ y6 = copy.deepcopy(x)
 
 x[0][0] = 111
 
-print(x, y1, y2, y3, y4, y5, y6)
+print("x:", x, "\ny1:", y1, "\ny2:", y2, "\ny3:", y3, "\ny4:", y4, "\ny5:", y5, "\ny6:", y6)
+
+id(x)
+id(x[0])
+id(y1[0])
+id(y2[0])
+id(y6[0])
+
+
+
+x = [1, 2, 3]
+y1 = x
+y2 = x.copy()
+y3 = list(x)
+y4 = copy.copy(x)
+y5 = x[:]
+y6 = copy.deepcopy(x)
+
+x[0] = 111
+
+print("x:", x, "\ny1:", y1, "\ny2:", y2, "\ny3:", y3, "\ny4:", y4, "\ny5:", y5, "\ny6:", y6)
